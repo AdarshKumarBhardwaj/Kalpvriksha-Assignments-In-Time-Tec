28b13caa67d0e527e01abddca57e713f080309b6
AdarshKumarBhardwaj <129866424+AdarshKumarBhardwaj@users.noreply.github.com>
2025-01-14 08:30:30 +0530

    updated function arguments

--- LargestAndSmallestWordInSentence.c
+++ LargestAndSmallestWordInSentence.c
@@ -5,21 +5,29 @@
 #define MAX_LENGTH 200
 #define MAX_WORDS 100
 
-int stringLength(char *string){
-    int length=0;
-    int index=0;
-    while(string[index]!='\0'){
+int stringLength(char *string)
+{
+    int length = 0;
+    int index = 0;
+    while (string[index] != '\0')
+    {
         length++;
         index++;
     }
     return length;
 }
 
-void printWords(char sentence[], int startIndexArray[], int lastIndexArray[], int wordCount, int targetLength) {
-    for (int index1 = 0; index1 < wordCount; index1++) {
+void printLargestWords(char sentence[], int startIndexArray[], int lastIndexArray[], int dims[])
+{
+    int wordCount = dims[0];
+    int targetLength = dims[1];
+    for (int index1 = 0; index1 < wordCount; index1++)
+    {
         int length = lastIndexArray[index1] - startIndexArray[index1] + 1;
-        if (length == targetLength) {
-            for (int index2 = startIndexArray[index1]; index2 <= lastIndexArray[index1]; index2++) {
+        if (length == targetLength)
+        {
+            for (int index2 = startIndexArray[index1]; index2 <= lastIndexArray[index1]; index2++)
+            {
                 printf("%c", sentence[index2]);
             }
             printf("\n");
@@ -27,13 +35,33 @@ void printWords(char sentence[], int startIndexArray[], int lastIndexArray[], in
     }
 }
 
-void findLongestAndShortestWords(char sentence[]) {
+void printSmallestWords(char sentence[], int startIndexArray[], int lastIndexArray[], int dims[])
+{
+    int wordCount = dims[0];
+    int targetLength = dims[2];
+    for (int index1 = 0; index1 < wordCount; index1++)
+    {
+        int length = lastIndexArray[index1] - startIndexArray[index1] + 1;
+        if (length == targetLength)
+        {
+            for (int index2 = startIndexArray[index1]; index2 <= lastIndexArray[index1]; index2++)
+            {
+                printf("%c", sentence[index2]);
+            }
+            printf("\n");
+        }
+    }
+}
+
+void findLongestAndShortestWords(char sentence[])
+{
     int startIndexArray[MAX_WORDS];
     int lastIndexArray[MAX_WORDS];
     int wordCount = 0;
 
     char *token = strtok(sentence, " \n");
-    while (token != NULL) {
+    while (token != NULL)
+    {
         int startIndex = token - sentence;
         int length = stringLength(token);
 
@@ -44,32 +72,38 @@ void findLongestAndShortestWords(char sentence[]) {
         token = strtok(NULL, " \n");
     }
 
-    if (wordCount == 0) {
+    if (wordCount == 0)
+    {
         printf("No word found in the input.\n");
     }
-    else{
-    int longestLength = 0;
-    int smallestLength = MAX_LENGTH;
+    else
+    {
+        int longestLength = 0;
+        int smallestLength = MAX_LENGTH;
 
-    for (int index = 0; index < wordCount; index++) {
-        int length = lastIndexArray[index] - startIndexArray[index] + 1;
-        if (length > longestLength) {
-            longestLength = length;
+        for (int index = 0; index < wordCount; index++)
+        {
+            int length = lastIndexArray[index] - startIndexArray[index] + 1;
+            if (length > longestLength)
+            {
+                longestLength = length;
+            }
+            if (length < smallestLength)
+            {
+                smallestLength = length;
+            }
         }
-        if (length < smallestLength) {
-            smallestLength = length;
-        }
-    }
+        int dims[] = {wordCount, longestLength, smallestLength};
+        printf("Longest Words:\n");
+        printLargestWords(sentence, startIndexArray, lastIndexArray, dims);
 
-    printf("Longest Words:\n");
-    printWords(sentence, startIndexArray, lastIndexArray, wordCount, longestLength);
-
-    printf("Shortest Words:\n");
-    printWords(sentence,startIndexArray, lastIndexArray, wordCount, smallestLength);
+        printf("Shortest Words:\n");
+        printSmallestWords(sentence, startIndexArray, lastIndexArray, dims);
     }
 }
 
-int main() {
+int main()
+{
     char sentence[MAX_LENGTH];
 
     printf("Enter the sentence:\n");
@@ -79,4 +113,3 @@ int main() {
 
     return 0;
 }
-
