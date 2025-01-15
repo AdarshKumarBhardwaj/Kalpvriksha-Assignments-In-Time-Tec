9ebef1284570de30686fff86a22620a87050739c
AdarshKumarBhardwaj <129866424+AdarshKumarBhardwaj@users.noreply.github.com>
2025-01-15 21:09:06 +0530

    Modularise this code

--- kthElementInSortedMatrix.c
+++ kthElementInSortedMatrix.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int countLessEqual(int **matrix, int row, int column, int mid)
 {
     int count = 0;
@@ -20,6 +21,7 @@ int countLessEqual(int **matrix, int row, int column, int mid)
     }
     return count;
 }
+
 int kthSmallestElements(int **matrix, int row, int column, int k)
 {
     int low = matrix[0][0];
@@ -39,14 +41,9 @@ int kthSmallestElements(int **matrix, int row, int column, int k)
     }
     return low;
 }
-int main()
-{
-    int row, column, k;
-    printf("Enter the no of row\n");
-    scanf("%d", &row);
-    printf("Enter the no of column\n");
-    scanf("%d", &column);
 
+int **getMatrixInput(int row, int column)
+{
     int **matrix = (int **)malloc(row * sizeof(int *));
     for (int index = 0; index < row; index++)
     {
@@ -57,11 +54,16 @@ int main()
     {
         for (int colIndex = 0; colIndex < column; colIndex++)
         {
-            printf("Enter the element at (%d,%d):- ", rowIndex, colIndex);
+            printf("Enter the element at (%d,%d): ", rowIndex, colIndex);
             scanf("%d", &matrix[rowIndex][colIndex]);
         }
     }
-    printf("Matrix Elements are\n");
+    return matrix;
+}
+
+void printMatrix(int **matrix, int row, int column)
+{
+    printf("Matrix Elements are:\n");
     for (int rowIndex = 0; rowIndex < row; rowIndex++)
     {
         for (int colIndex = 0; colIndex < column; colIndex++)
@@ -70,16 +72,34 @@ int main()
         }
         printf("\n");
     }
+}
 
-    printf("Enter the value of k\n");
-    scanf("%d", &k);
-    int result = kthSmallestElements(matrix, row, column, k);
-    printf("%d smallest element of matrix is %d ", k, result);
-
+void freeMatrix(int **matrix, int row)
+{
     for (int index = 0; index < row; index++)
     {
         free(matrix[index]);
     }
     free(matrix);
+}
+
+int main()
+{
+    int row, column, k;
+    printf("Enter the number of rows: ");
+    scanf("%d", &row);
+    printf("Enter the number of columns: ");
+    scanf("%d", &column);
+
+    int **matrix = getMatrixInput(row, column);
+    printMatrix(matrix, row, column);
+
+    printf("Enter the value of k: ");
+    scanf("%d", &k);
+
+    int result = kthSmallestElements(matrix, row, column, k);
+    printf("%d smallest element of the matrix is %d\n", k, result);
+
+    freeMatrix(matrix, row);
     return 0;
 }
