80d4ae4e65b48f94c0564ef38aa39167499a4735
Adarsh Kumar Bhardwaj <129866424+AdarshKumarBhardwaj@users.noreply.github.com>
2025-01-27 22:40:24 +0530

    Removed review comments

--- flattenLL.c
+++ flattenLL.c
@@ -1,102 +1,139 @@
 #include <stdio.h>
-#include<stdlib.h>
- 
-struct node 
+#include <stdlib.h>
+
+struct node
 {
     int data;
     struct node *next;
     struct node *child;
 };
- 
-struct node * flatten(struct node *head){
-    if(!head) return NULL;
-    if(!head->child) {
-        if(head->next) flatten(head->next);  
+
+struct node *flatten(struct node *head)
+{
+    if (!head)
+    {
+        return NULL;
+    }
+
+    if (!head->child)
+    {
+        if (head->next)
+            flatten(head->next);
         return head;
     }
-    struct node * nextNode = head->next; 
-    struct node *childNode = flatten(head->child);  
+    struct node *nextNode = head->next;
+    struct node *childNode = flatten(head->child);
     head->next = childNode;
-    while(childNode && childNode->next){
+
+    while (childNode && childNode->next)
+    {
         childNode = childNode->next;
     }
     childNode->next = nextNode;
     flatten(nextNode);
     return head;
 }
- 
-void print(struct node * head){
-    if(!head) return ;
-    
-    printf("%d\n",head->data);
-    printf("child of %d:",head->data);
-    if(head->child){
-        printf("%d\n",head->child->data);
+
+void print(struct node *head)
+{
+    if (!head)
+        return;
+
+    printf("%d\n", head->data);
+    printf("child of %d:", head->data);
+    if (head->child)
+    {
+        printf("%d\n", head->child->data);
     }
-    else printf("\n");
-    printf("next of %d:",head->data);
-    if(head->next){
-        printf("%d\n",head->next->data);
+    else
+    {
+        printf("\n");
     }
-    else printf("\n");
+    printf("next of %d:", head->data);
+    if (head->next)
+    {
+        printf("%d\n", head->next->data);
+    }
+    else
+    {
+        printf("\n");
+    }
     print(head->child);
     print(head->next);
 }
- 
-void printnormi(struct node * head){
+
+void printFlattenLL(struct node *head)
+{
     struct node *temp = head;
-    while(temp){
-        printf("%d",temp->data);
+    while (temp)
+    {
+        printf("%d ", temp->data);
         temp = temp->next;
     }
+    printf("\n");
 }
- 
- 
-void createLinkedList(struct node *head,int n, int num){
-    if(num == n) return ;
-    printf("enter child of %d\n",head->data);
-    struct node *temp1 = malloc(sizeof(struct node));
-     int data1;
-    scanf("%d",&data1);
-    if(data1 != -1)
+
+void createLinkedList(struct node *head, int numberOfNodes, int count)
+{
+    if (count == numberOfNodes)
+        return;
+    if (count == 0)
     {
-    temp1->data = data1;
-    head->child = temp1;
-    num++;
-    }else
+        printf("Enter the head node:\n");
+        scanf("%d", &head->data);
+    }
+    printf("Enter child of %d\n", head->data);
+    struct node *newChildNode = malloc(sizeof(struct node));
+    int data1;
+    scanf("%d", &data1);
+    if (data1 != -1)
+    {
+        newChildNode->data = data1;
+        head->child = newChildNode;
+        count++;
+    }
+    else
     {
         head->child = NULL;
     }
-    printf("enter next of %d\n",head->data);
-    struct node *temp = malloc(sizeof(struct node));
- 
+    printf("Enter next of %d\n", head->data);
+    struct node *newNextNode = malloc(sizeof(struct node));
+
     int data;
-    scanf("%d",&data);
-    if(data != -1){
-    temp->data = data;
-    head->next = temp;
-    num++;
-    }else{
+    scanf("%d", &data);
+    if (data != -1)
+    {
+        newNextNode->data = data;
+        head->next = newNextNode;
+        count++;
+    }
+    else
+    {
         head->next = NULL;
     }
-    if(head->child)
-    createLinkedList(head->child,n,num);
-    if(head->next)
-    createLinkedList(head->next,n,num);
-
+    if (head->child)
+        createLinkedList(head->child, numberOfNodes, count);
+    if (head->next)
+        createLinkedList(head->next, numberOfNodes, count);
 }
- 
-int main() {
+
+int main()
+{
+    int numberOfNodes;
     printf("Enter the number of nodes\n");
-    int num;
-    scanf("%d",&num);
+    scanf("%d", &numberOfNodes);
+
     struct node *head = malloc(sizeof(struct node));
-    printf("Enter the head node:\n");
-    scanf("%d",&head->data);
-    createLinkedList(head,num,0);
+    createLinkedList(head, numberOfNodes, 0);
+
+    printf("\nOriginal Multi-Level Linked List:\n");
     print(head);
+
     flatten(head);
-    printnormi(head);
+
+    printf("\nFlattened Linked List:\n");
+    printFlattenLL(head);
+
     return 0;
 }
 
