23d7bfa781ddecc8005fe8b337f9be92e68cb8da
Adarsh Kumar Bhardwaj <129866424+AdarshKumarBhardwaj@users.noreply.github.com>
2025-02-03 14:14:06 +0530

    Added Question in file

--- stack_queue_practice_2.c
+++ stack_queue_practice_2.c
@@ -1,3 +1,13 @@
+// Nearest Smaller Element 
+// Problem Statement: Given an array, find the nearest smaller element G[i] for every element A[i] in the array such that the element has an index smaller than i. 
+
+// More formally, 
+//    G[i] for an element A[i] = an element A[j] such that  
+//    j is maximum possible AND  
+//    j < i AND 
+//    A[j] < A[i] 
+// Elements for which no smaller element exist, consider next smaller element as -1.
+
 #include <stdio.h>
 #include <stdlib.h>
 #define size 15
